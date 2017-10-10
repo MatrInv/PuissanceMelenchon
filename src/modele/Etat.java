8ff6d665e35619a80264ae4dd5e882f7f4cbcb54
Augustin G <giovinazzoaugustin@gmail.com>
2017-10-10 17:53:26 +0200

    Go !

--- src/modele/Etat.java
+++ src/modele/Etat.java
@@ -1,5 +1,18 @@
 package modele;
 
-public class Etat {
+import java.util.Set;
 
+public class Etat {
+	
+	private int taille;
+	private int[][] plateau;
+	
+	private Etat pere;
+	private Set<Etat> fils;
+	
+	
+	public Etat(int t){
+		taille=t;
+		plateau=new int[][]();
+	}
 }
