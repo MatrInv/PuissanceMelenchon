cafa86e26019e3efe26d4ac2c10721b0fbc0ba32
matrinv <giovinazzoaugustin@gmail.com>
2017-10-11 15:59:51 +0200

    Ajout de fonctions (getNbLig, getNbCol, jouerCol, colJouable) dans la classe Etat.

--- src/modele/Etat.java
+++ src/modele/Etat.java
@@ -14,15 +14,38 @@ public class Etat {
 	private Set<Etat> fils;
 	
 	
-	public Etat(int h, int l){
+	public Etat(int l, int h){
 		//hauteur=h;
 		//largeur=l;
 		
-		plateau=new int[h][l];
-		for(int i=0;i<=h;i++){
-			for(int j=0;j<=l;j++){
+		plateau=new int[l][h];
+		for(int i=0;i<=l;i++){
+			for(int j=0;j<=h;j++){
 				plateau[i][j]=0;
 			}
 		}
-	}	
+	}
+	
+	private int getNbCol(){
+		return plateau.length;
+	}
+	
+	private int getNbLig(){
+		return plateau[0].length;
+	}
+	
+	private void jouerCol(int noCol, int player){
+		if(colJouable(noCol)){
+			int next=0;
+			while(plateau[noCol][next]==0){
+				next++;
+			}
+			plateau[noCol][next-1]=player;
+		}
+	}
+	
+	private boolean colJouable(int col){
+		return (col<getNbCol() && plateau[col][getNbLig()-1]==0);
+	}
+	
 }
